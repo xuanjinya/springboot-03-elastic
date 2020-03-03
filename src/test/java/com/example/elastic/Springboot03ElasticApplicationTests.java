a4d843088c3d3957a9afb1430106fbf82906ca74
xuanjinya <1067998193@qq.com>
2020-03-03 14:19:40 +0800

    给ES添加一个内容

--- src/test/java/com/example/elastic/Springboot03ElasticApplicationTests.java
+++ src/test/java/com/example/elastic/Springboot03ElasticApplicationTests.java
@@ -1,16 +1,40 @@
 package com.example.elastic;
 
+import com.example.elastic.bean.Article;
+import io.searchbox.client.JestClient;
+import io.searchbox.core.Index;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class Springboot03ElasticApplicationTests {
 
+    @Autowired
+    JestClient jestClient;
+
     @Test
     public void contextLoads() {
+        //1.给ES中索引保存一个文档
+        Article article = new Article();
+        article.setId(1);
+        article.setTitle("好消息");
+        article.setAuthor("张三");
+        article.setContent("Hello world");
+        article.setContent("Hello world");
+
+        //构建一个索引
+        Index index = new Index.Builder(article).index("atguigu").type("news").build();
+        try {
+            jestClient.execute(index);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
